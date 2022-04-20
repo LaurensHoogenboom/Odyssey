f63dd7124cdb24645db956a727be406140009a0c
LaurensH2000 <49717394+LaurensHoogenboom@users.noreply.github.com>
2022-04-20 16:14:45 +0200

    connect to bluetooth device

--- game/js/ergo.js
+++ game/js/ergo.js
@@ -168,7 +168,7 @@ function addScoreForTree(tree_id) {
 }
 
 function updateScoreDisplay() {
-    scoreDisplay.setAttribute('visible', true);
+    scoreDisplay.setAttribute('visible', true)
     scoreDisplay.setAttribute('value', score)
 }
 
@@ -179,48 +179,110 @@ let menuGameOver
 let menuContainer
 let startButton
 let restartButton
+let bluetoothMenu
+let arButton
+let vrButton
+let connectionSection
+let enterGameSection
+let enterGameButton
 
 function hideEntity(el) {
-    el.setAttribute('visible', false);
+    el.setAttribute('visible', false)
 }
 
 function showEntity(el) {
-    el.setAttribute('visible', true);
+    el.setAttribute('visible', true)
+}
+
+function hideHTML(el) {
+    el.classList.add('hidden')
+}
+
+function showHTML(el) {
+    el.classList.remove('hidden')
 }
 
 function setupAllMenus() {
-    menuStart = document.getElementById('start-menu');
-    menuGameOver = document.getElementById('game-over');
-    menuContainer = document.getElementById('menu-container');
-    startButton = document.getElementById('start-button');
-    restartButton = document.getElementById('restart-button');
+    menuStart = document.getElementById('start-menu')
+    menuGameOver = document.getElementById('game-over')
+    menuContainer = document.getElementById('menu-container')
+    startButton = document.getElementById('start-button')
+    restartButton = document.getElementById('restart-button')
+    bluetoothMenu = document.getElementById('bluetooth-menu')
+    arButton = document.querySelector('.a-enter-ar-button')
+    vrButton = document.querySelector('.a-enter-vr-button')
+    connectionSection = document.getElementById('connection-section')
+    enterGameSection = document.getElementById('enter-game-section')
+    enterGameButton = document.getElementById('enter-game')
+    playerCamera = document.getElementById('player-camera')
 
-    startButton.addEventListener('click', startGame);
-    restartButton.addEventListener('click', startGame);
+    enterGameButton.addEventListener('click', enterGame)
+    startButton.addEventListener('click', startGame)
+    restartButton.addEventListener('click', startGame)
 
-    showStartMenu();
+    hideAllMenus()
+    showBluetoothMenu()
 }
 
 function hideAllMenus() {
-    hideEntity(menuContainer);
-    startButton.classList.remove('clickable');
-    restartButton.classList.remove('clickable');
+    hideEntity(menuContainer)
+    hideBluetoothMenu()
+    enterGameButton.style.pointerEvents = 'none'
+    startButton.classList.remove('clickable')
+    restartButton.classList.remove('clickable')
 }
 
 function showGameOverMenu() {
-    showEntity(menuContainer);
-    hideEntity(menuStart);
-    showEntity(menuGameOver);
-    startButton.classList.remove('clickable');
-    restartButton.classList.add('clickable');
+    showEntity(menuContainer)
+    hideEntity(menuStart)
+    showEntity(menuGameOver)
+    startButton.classList.remove('clickable')
+    restartButton.classList.add('clickable')
 }
 
 function showStartMenu() {
-    showEntity(menuContainer);
-    hideEntity(menuGameOver);
-    showEntity(menuStart);
-    startButton.classList.add('clickable');
-    restartButton.classList.remove('clickable');
+    showEntity(menuContainer)
+    hideEntity(menuGameOver)
+    showEntity(menuStart)
+    startButton.classList.add('clickable')
+    restartButton.classList.remove('clickable')
+}
+
+const showBluetoothMenu = () => {
+    const bluetooth = new BluetoothController(handleReceivedBluetoothData)
+
+    showHTML(bluetoothMenu)
+    hideHTML(arButton)
+    hideHTML(vrButton)
+    disableLookControls()
+
+    bluetoothMenu.classList.remove('hidden')
+
+    if (isConnectedToDevice) {
+        hideHTML(connectionSection)
+        showHTML(enterGameSection)
+        enterGameButton.style.pointerEvents = 'all'
+    } else {
+        hideHTML(enterGameSection)
+        showHTML(connectionSection)
+        enterGameButton.style.pointerEvents = 'none'
+    }
+}
+
+const hideBluetoothMenu = () => {
+    showHTML(arButton)
+    showHTML(vrButton)
+    hideHTML(bluetoothMenu)
+    enterGameButton.style.pointerEvents = 'none'
+    enableLookControls()
+}
+
+const enableLookControls = () => {
+    playerCamera.setAttribute('look-controls', 'enabled: true')
+}
+
+const disableLookControls = () => {
+    playerCamera.setAttribute('look-controls', 'enabled: false')
 }
 
 //game
@@ -234,6 +296,11 @@ window.onload = () => {
     setupAllMenus()
     setupScore()
     setupTrees()
+}
+
+const enterGame = () => {
+    hideBluetoothMenu()
+    showStartMenu()
 }
 
 function startGame() {
@@ -252,6 +319,21 @@ function gameOver() {
     tearDownTrees()
     tearDownScore()
     showGameOverMenu()
+}
+
+//bluetooth
+
+let isConnectedToDevice = false
+
+const handleReceivedBluetoothData = (data) => {
+    console.log(data)
+
+    if (data == 'CONNECTED!') {
+        isConnectedToDevice = true
+
+        showBluetoothMenu()
+        console.log('Connected! Ready to start.')
+    }
 }
 
 //utilities
