1ef74d5f3e090ca4367146f93dac24b318899cf6
LaurensH2000 <49717394+LaurensHoogenboom@users.noreply.github.com>
2022-04-21 16:50:38 +0200

    cache handling better responsiveness

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
-const cache_name = 'web-app-cache-v1.0'
-const filesToCache = [
+const PRECACHE = 'odyssey-cache-v1.0'
+const RUNTIME = 'runtime'
+const PRECAHCE_URLS = [
     '/index.html',
     '/service-worker.js',
     '/site.webmanifest',
@@ -16,9 +17,53 @@ const filesToCache = [
 
 self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open(cache_name).then((cache) => {
-            //Cache has been opened succesfully
-            return cache.addAll(filesToCache)
+        caches.open(PRECACHE).then((cache) => {
+            return cache.addAll(PRECAHCE_URLS)
         })
     )
+})
+
+self.addEventListener('activate', (event) => {
+    const currentCaches = [PRECACHE, RUNTIME]
+    event.waitUntil(
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter(
+                    (cacheName) => !currentCaches.includes(cacheName)
+                )
+            })
+            .then((cachesToDelete) => {
+                return Promise.all(
+                    cachesToDelete.map((cacheToDelete) => {
+                        return caches.delete(cacheToDelete)
+                    })
+                )
+            })
+            .then(() => self.clients.claim())
+    )
+})
+
+self.addEventListener('fetch', (event) => {
+    // Skip cross-origin requests, like those for Google Analytics.
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse
+                }
+
+                return caches.open(RUNTIME).then((cache) => {
+                    return fetch(event.request).then((response) => {
+                        // Put a copy of the response in the runtime cache.
+                        return cache
+                            .put(event.request, response.clone())
+                            .then(() => {
+                                return response
+                            })
+                    })
+                })
+            })
+        )
+    }
 })
