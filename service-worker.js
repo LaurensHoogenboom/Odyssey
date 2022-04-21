ea4fb46a8e3c2c7967adc78dc6e2b96f0da24611
LaurensH2000 <49717394+LaurensHoogenboom@users.noreply.github.com>
2022-04-21 16:17:04 +0200

    pwa

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,43 @@
-const web_cache = 'web-app-cache-v1.0'
-const filesToCache = ['/']
+const cache_name = 'web-app-cache-v1.0'
+const filesToCache = [
+    '/index.html',
+    '/service-worker.js',
+    '/site.webmanifest',
+    '/src/css/style.css',
+    '/src/font/exo-2.woff',
+    '/src/font/Exo2Bold.fnt',
+    '/src/font/Exo2Bold.png',
+    '/src/js/bluetooth.js',
+    '/src/js/ergo.js',
+    '/src/js/low-poly.js',
+    '/src/js/ocean.js',
+    '/src/js/aframe.js',
+]
 
 self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open(web_cache).then((cache) => {
+        caches.open(cache_name).then((cache) => {
             //Cache has been opened succesfully
             return cache.addAll(filesToCache)
         })
+    )
+})
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        (async () => {
+            const r = await caches.match(e.request)
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}`)
+            if (r) {
+                return r
+            }
+            const response = await fetch(e.request)
+            const cache = await caches.open(cache_name)
+            console.log(
+                `[Service Worker] Caching new resource: ${e.request.url}`
+            )
+            cache.put(e.request, response.clone())
+            return response
+        })()
     )
 })
