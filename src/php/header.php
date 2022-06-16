59e29d2f6c0b5848ea9e24463fbf25afc05761ad
LaurensH2000 <49717394+LaurensHoogenboom@users.noreply.github.com>
2022-06-16 19:24:43 +0200

    new fav icons

--- src/php/header.php
+++ src/php/header.php
@@ -26,7 +26,7 @@
         <script src="/src/js/progress.js"></script>
         <script src="/src/js/game.js"></script>
 
-        <!-- <script>
+        <script>
             if ('serviceWorker' in navigator) {
                 window.addEventListener('load', function () {
                     navigator.serviceWorker.register('/service-worker.js', {
@@ -34,20 +34,21 @@
                     })
                 })
             }
-        </script> -->
+        </script>
 
-        <link rel="apple-touch-icon" sizes="180x180" href="src/images/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="src/images/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="src/images/favicons/favicon-16x16.png" />
-        <!-- <link rel="manifest" href="site.webmanifest" /> -->
-        <link rel="mask-icon" href="src/images/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-        <link rel="shortcut icon" href="src/images/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/src/images/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/src/images/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/src/images/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/src/images/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/src/images/favicons/favicon.ico" />
         <meta name="apple-mobile-web-app-title" content="Odyssey" />
         <meta name="application-name" content="Odyssey" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="msapplication-config" content="src/images/favicons/browserconfig.xml" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="msapplication-TileColor" content="#2d89ef" />
+        <meta name="msapplication-config" content="/src/images/favicons/browserconfig.xml" />
+        <meta name="theme-color" content="#a3d0ed" />
 
         <link rel="stylesheet" href="/src/css/style.css" />
     </head>
     <body>
+
